697acfc28b6275ab5c162430c116096706766afd
Baptiste Candellier <baptiste@candellier.me>
2024-05-01 13:38:38 +0200

    Extract file

--- app/src/main/java/fr/outadoc/pictochat/DeviceNameProvider.kt
+++ app/src/main/java/fr/outadoc/pictochat/DeviceNameProvider.kt
@@ -1,40 +1,5 @@
 package fr.outadoc.pictochat
 
-import android.Manifest
-import android.bluetooth.BluetoothManager
-import android.content.Context
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.core.content.getSystemService
-
 interface DeviceNameProvider {
     fun getDeviceName(): String
-}
-
-/**
- * Returns the name of the device, using the Bluetooth APIs, and falling back on the device's model.
- */
-class AndroidDeviceNameProvider(
-    private val applicationContext: Context,
-) : DeviceNameProvider {
-
-    override fun getDeviceName(): String {
-        val permissionName = if (Build.VERSION.SDK_INT >= 31) {
-            Manifest.permission.BLUETOOTH_CONNECT
-        } else {
-            Manifest.permission.BLUETOOTH
-        }
-
-        val bluetoothPermission: Int = applicationContext.checkSelfPermission(permissionName)
-
-        val bluetoothName: String? =
-            if (bluetoothPermission == PackageManager.PERMISSION_GRANTED) {
-                val bluetoothManager = applicationContext.getSystemService<BluetoothManager>()
-                bluetoothManager?.adapter?.name
-            } else {
-                null
-            }
-
-        return bluetoothName ?: "${Build.MANUFACTURER} ${Build.MODEL}"
-    }
 }
