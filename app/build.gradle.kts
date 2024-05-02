9161eccfea2ecdddce72aaeca337f4f3dba0bf55
Baptiste Candellier <baptiste@candellier.me>
2024-05-02 19:40:26 +0200

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,19 +31,24 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.13"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
