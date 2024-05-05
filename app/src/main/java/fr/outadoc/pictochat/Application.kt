224d96939e0d1d18869b7b367c06c7ce16938306
Baptiste Candellier <baptiste@candellier.me>
2024-05-05 23:44:30 +0200

    Implement settings screen properly

--- app/src/main/java/fr/outadoc/pictochat/Application.kt
+++ app/src/main/java/fr/outadoc/pictochat/Application.kt
@@ -7,18 +7,15 @@ import fr.outadoc.pictochat.data.NearbyConnectionManager
 import fr.outadoc.pictochat.data.NearbyLobbyManager
 import fr.outadoc.pictochat.domain.ConnectionManager
 import fr.outadoc.pictochat.domain.LobbyManager
-import fr.outadoc.pictochat.domain.ProfileColor
 import fr.outadoc.pictochat.preferences.AndroidDeviceNameProvider
 import fr.outadoc.pictochat.preferences.DataStoreDeviceIdProvider
+import fr.outadoc.pictochat.preferences.DataStoreLocalPreferencesRepository
 import fr.outadoc.pictochat.preferences.DeviceIdProvider
 import fr.outadoc.pictochat.preferences.DeviceNameProvider
-import fr.outadoc.pictochat.preferences.LocalPreferences
 import fr.outadoc.pictochat.preferences.LocalPreferencesRepository
-import fr.outadoc.pictochat.preferences.UserProfile
 import fr.outadoc.pictochat.ui.navigation.MainViewModel
 import fr.outadoc.pictochat.ui.room.RoomViewModel
 import fr.outadoc.pictochat.ui.settings.SettingsViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.datetime.Clock
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -43,20 +40,7 @@ class Application : Application() {
         single<DeviceIdProvider> { DataStoreDeviceIdProvider(get()) }
         single<ConnectionManager> { NearbyConnectionManager(get(), get()) }
         single<LobbyManager> { NearbyLobbyManager(get(), get(), get(), get()) }
-        single<LocalPreferencesRepository> {
-            object : LocalPreferencesRepository {
-                override val preferences = MutableStateFlow(
-                    LocalPreferences(
-                        userProfile = UserProfile(
-                            displayName = get<DeviceNameProvider>().getDeviceName(),
-                            displayColor = ProfileColor.Color1
-                        )
-                    )
-                )
-
-                override suspend fun updatePreferences(updated: LocalPreferences) {}
-            }
-        }
+        single<LocalPreferencesRepository> { DataStoreLocalPreferencesRepository(get(), get()) }
 
         single { MainViewModel(get()) }
         single { RoomViewModel(get()) }
--- app/src/main/java/fr/outadoc/pictochat/ui/settings/SettingsScreen.kt
+++ app/src/main/java/fr/outadoc/pictochat/ui/settings/SettingsScreen.kt
@@ -1,24 +1,37 @@
 package fr.outadoc.pictochat.ui.settings
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import fr.outadoc.pictochat.domain.ProfileColor
@@ -86,25 +99,131 @@ fun SettingsContent(
         }
 
         item {
+            var openDialog by remember { mutableStateOf(false) }
+
             SettingsText(
                 modifier = Modifier.padding(horizontal = 16.dp),
                 title = { Text("Display name") },
-                subtitle = { Text(preferences.userProfile.displayName) }
+                subtitle = { Text(preferences.userProfile.displayName) },
+                onClick = {
+                    openDialog = true
+                },
+                onClickLabel = "Change display name"
             )
+
+            if (openDialog) {
+                var editValue by remember { mutableStateOf(preferences.userProfile.displayName) }
+
+                AlertDialog(
+                    title = { Text(text = "What's your name?") },
+                    text = {
+                        TextField(
+                            value = editValue,
+                            onValueChange = {
+                                editValue = it
+                            }
+                        )
+                    },
+                    onDismissRequest = { openDialog = false },
+                    confirmButton = {
+                        TextButton(
+                            onClick = {
+                                onUpdatePreferences(
+                                    preferences.copy(
+                                        userProfile = preferences.userProfile.copy(
+                                            displayName = editValue
+                                        )
+                                    )
+                                )
+                                openDialog = false
+                            }
+                        ) {
+                            Text("Confirm")
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(onClick = { openDialog = false }) {
+                            Text("Dismiss")
+                        }
+                    }
+                )
+            }
         }
 
-        items(ProfileColor.entries) { item ->
+        item {
+            var openDialog by remember { mutableStateOf(false) }
+
             SettingsText(
                 modifier = Modifier.padding(horizontal = 16.dp),
                 title = { Text("Favorite color") },
+                onClick = { openDialog = true },
                 trailingIcon = {
                     Surface(
                         modifier = Modifier.size(32.dp),
                         shape = CircleShape,
-                        color = item.toColor()
+                        color = preferences.userProfile.displayColor.toColor()
                     ) {}
-                }
+                },
             )
+
+            if (openDialog) {
+                var selectedColor by remember { mutableStateOf(preferences.userProfile.displayColor) }
+
+                AlertDialog(
+                    title = { Text(text = "What's your favorite color?") },
+                    text = {
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(4),
+                            horizontalArrangement = Arrangement.spacedBy(16.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            items(ProfileColor.entries) { color ->
+                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                    Surface(
+                                        modifier = Modifier.size(32.dp),
+                                        shape = CircleShape,
+                                        color = color.toColor()
+                                    ) {
+                                        IconButton(onClick = {
+                                            selectedColor = color
+                                        }) {
+                                            if (selectedColor == color) {
+                                                Icon(
+                                                    Icons.Default.Check,
+                                                    contentDescription = "Selected",
+                                                    tint = MaterialTheme.colorScheme.onPrimary
+                                                )
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    onDismissRequest = { openDialog = false },
+                    confirmButton = {
+                        TextButton(
+                            onClick = {
+                                onUpdatePreferences(
+                                    preferences.copy(
+                                        userProfile = preferences.userProfile.copy(
+                                            displayColor = selectedColor
+                                        )
+                                    )
+                                )
+                                openDialog = false
+                            }
+                        ) {
+                            Text("Confirm")
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(onClick = { openDialog = false }) {
+                            Text("Dismiss")
+                        }
+                    }
+                )
+            }
         }
     }
 }
