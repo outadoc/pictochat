4ff4e1cfd3680128865509bd5b255a27e0c14ad3
Baptiste Candellier <baptiste@candellier.me>
2024-05-06 21:29:32 +0200

    Improve permissions screen

--- app/src/main/java/fr/outadoc/pictochat/ui/permissionlock/PermissionLockedScreen.kt
+++ app/src/main/java/fr/outadoc/pictochat/ui/permissionlock/PermissionLockedScreen.kt
@@ -2,17 +2,23 @@ package fr.outadoc.pictochat.ui.permissionlock
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.MultiplePermissionsState
-import com.google.accompanist.permissions.PermissionState
 
-@OptIn(ExperimentalPermissionsApi::class)
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun PermissionLocked(
     multiplePermissionsState: MultiplePermissionsState,
@@ -21,60 +27,40 @@ fun PermissionLocked(
     if (multiplePermissionsState.allPermissionsGranted) {
         body()
     } else {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Text(
-                getTextToShowGivenPermissions(
-                    permissions = multiplePermissionsState.revokedPermissions,
-                    shouldShowRationale = multiplePermissionsState.shouldShowRationale
+        Scaffold { insets ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(insets)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier.padding(bottom = 16.dp),
+                    text = "Permissions required",
+                    style = MaterialTheme.typography.headlineMedium
                 )
-            )
 
-            Button(onClick = { multiplePermissionsState.launchMultiplePermissionRequest() }) {
-                Text("Request permissions")
-            }
-        }
-    }
-}
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .padding(bottom = 16.dp),
+                    textAlign = TextAlign.Center,
+                    text = buildString {
+                        appendLine(
+                            "This app needs permissions to discover the devices near you and chat with them."
+                        )
+                        if (multiplePermissionsState.shouldShowRationale) {
+                            appendLine("Some permissions were denied. The app cannot function without them.")
+                        }
+                    }
+                )
 
-@OptIn(ExperimentalPermissionsApi::class)
-private fun getTextToShowGivenPermissions(
-    permissions: List<PermissionState>,
-    shouldShowRationale: Boolean,
-): String {
-    val revokedPermissionsSize = permissions.size
-    if (revokedPermissionsSize == 0) return ""
-
-    return buildString {
-        append("The ")
-
-        permissions.indices.forEach { i ->
-            append(permissions[i].permission)
-
-            when {
-                revokedPermissionsSize > 1 && i == revokedPermissionsSize - 2 -> {
-                    append(", and ")
-                }
-
-                i == revokedPermissionsSize - 1 -> {
-                    append(" ")
-                }
-
-                else -> {
-                    append(", ")
+                Button(onClick = { multiplePermissionsState.launchMultiplePermissionRequest() }) {
+                    Text("Request permissions")
                 }
             }
         }
-
-        append(if (revokedPermissionsSize == 1) "permission is" else "permissions are")
-        append(
-            if (shouldShowRationale) {
-                " important. Please grant all of them for the app to function properly."
-            } else {
-                " denied. The app cannot function without them."
-            }
-        )
     }
 }
