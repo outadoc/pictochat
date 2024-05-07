a4c0c3b0acfcad5ae5faba788ffdf052bf73220a
Baptiste Candellier <baptiste@candellier.me>
2024-05-07 20:37:39 +0200

    Replace clear icon with trash icon

--- app/src/main/java/fr/outadoc/pictochat/ui/room/RoomInput.kt
+++ app/src/main/java/fr/outadoc/pictochat/ui/room/RoomInput.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
-import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Keyboard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -177,7 +177,7 @@ fun RoomInputActions(
 
         IconButton(onClick = onClearMessage) {
             Icon(
-                Icons.Default.Clear,
+                Icons.Default.Delete,
                 contentDescription = "Clear message"
             )
         }
